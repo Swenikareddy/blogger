c0b342d3457a271dafad418e9323f398c9c86010
Swenikareddy <71818894+Swenikareddy@users.noreply.github.com>
2021-07-30 14:52:25 +0530

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 from django.conf import settings
-from django.conf.urls.static import static
+#from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
 urlpatterns = [
@@ -26,6 +26,7 @@ urlpatterns = [
     path('members/',include('django.contrib.auth.urls')),
     path('members/',include('members.urls')),
     url(r'^media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT }),
-    url(r'^static/(?P<path>.*)$',serve,{'document_root': settings.STATIC_ROOT }),
+   # url(r'^static/(?P<path>.*)$',serve,{'document_root': settings.STATIC_ROOT }),
 
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+# static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
